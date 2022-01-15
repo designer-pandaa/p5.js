679cfdd5607dd3f6a68d609c87673e74bed593c2
designer-pandaa <97792597+designer-pandaa@users.noreply.github.com>
2022-01-15 19:06:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,76 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(275);
+  strokeWeight(0);
+  fill(125, 164, 120);
+  rect(150, 100, 100, 150, 60, 60, 0, 0);
+
+  //strokeWeight(0);
+  //fill(125, 70, 40);
+  fill(248, 129, 88);
+  quad(135, 350, 110, 250, 290, 250, 265, 350);
+
+  // strokeWeight(0);
+  //fill(125, 80, 50);
+  fill(230, 116, 81);
+  quad(135, 350, 118, 280, 282, 280, 265, 350);
+
+  //  strokeWeight(0);
+  fill(110, 140, 100);
+  rect(175, 100, 50, 150, 60, 60, 0, 0);
+
+  strokeWeight(5);
+  line(145, 170, 150, 175);
+  stroke(110, 140, 100);
+
+  //strokeWeight(5);
+  line(145, 200, 150, 205);
+  stroke(110, 140, 100);
+
+  // strokeWeight(5);
+  line(145, 230, 150, 235);
+  stroke(110, 140, 100);
+
+  //strokeWeight(5);
+  line(170, 170, 175, 175);
+  stroke(110, 140, 100);
+
+  // strokeWeight(5);
+  line(170, 200, 175, 205);
+  stroke(110, 140, 100);
+
+  // strokeWeight(5);
+  line(170, 230, 175, 235);
+  stroke(110, 140, 100);
+
+  //strokeWeight(5);
+  line(230, 170, 225, 175);
+  stroke(110, 140, 100);
+
+  //strokeWeight(5);
+  line(230, 200, 225, 205);
+  stroke(110, 140, 100);
+
+  // strokeWeight(5);
+  line(230, 230, 225, 235);
+  stroke(110, 140, 100);
+
+  //  strokeWeight(5);
+  line(170, 230, 175, 235);
+  stroke(110, 140, 100);
+
+  // strokeWeight(5);
+  line(255, 170, 250, 175);
+  stroke(110, 140, 100);
+
+  //  strokeWeight(5);
+  line(255, 200, 250, 205);
+  stroke(110, 140, 100);
+
+  // strokeWeight(5);
+  line(255, 230, 250, 235);
+  stroke(110, 140, 100);
+}
